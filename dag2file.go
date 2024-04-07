7e1232aba8af0770ca2cfb87a515f0fc112b586a
011wx <1726844758@qq.com>
2024-04-07 21:29:19 +0800

    实验二

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,38 @@
 package merkledag
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+	var data []byte 
+	var tree Object
+	h := hp.Get()
+	h.Write(hash)
+	value, err := store.Get(h.Sum(nil))
+	if err != nil {
+		_ = fmt.Errorf("get value from store failed, err: %s", err)
+		return nil
+	}
+
+
+	err = json.Unmarshal(value, &tree)
+	if err != nil {
+		_ = fmt.Errorf("unmarshal value to tree failed, err: %s", err)
+		return nil
+	}
+
+	
+	var start uint64 
+	var link Link
+	
+	for link = range tree.Links {
+		if link.Name == path {
+			data = tree.Data[start : start+link.Size]
+		}
+		start += link.Size
+	}
+	return data
 }
